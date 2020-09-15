bb62abdaa3d32febbe22533b66973f94ece59c3e
Raghu Anand <69553725+raghuanand16@users.noreply.github.com>
2020-09-15 09:42:18 +0000

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class Demo{
 	public static void main(String[] args){
 		System.out.println("Hi I am First line");
+		System.out.println("Hi I am Second line");
+		System.out.println("Hi I am Third line");
+		
 }
 }
